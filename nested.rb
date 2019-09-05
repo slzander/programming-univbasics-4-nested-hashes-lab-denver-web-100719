c85e3efb5b9481c6c097f86e5608e68b1f79f156
Stacey Zander <staceyzander@gmail.com>
2019-09-05 19:41:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -86,12 +86,11 @@ def adding_matz
         }
      }
 
-  programmer_hash[
+  programmer_hash[] =
     :yukihiro_matsumoto => {
       :known_for => "Ruby",
       :languages => ["LISP", "C"]
     }
-    ]
 end
 
 def changing_alan
