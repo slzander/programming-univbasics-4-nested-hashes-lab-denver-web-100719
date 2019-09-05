f633c2f6cafef770572284c1ba0d5af44e949b94
Stacey Zander <staceyzander@gmail.com>
2019-09-05 19:51:27 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -134,7 +134,7 @@ def adding_to_dennis
         }
      }
 
-  programmer_hash[:dennis_ritchie][:languages] += "Assembly"
+  programmer_hash[:dennis_ritchie][:languages].push "Assembly"
   return programmer_hash
 
 end
